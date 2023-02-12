b44884a0f9b66688652a0d4c558e91e1466bdc64
TTedy <breki121212@gmail.com>
2023-02-12 21:21:00 +0000

    359935

--- verkefni/app.py
+++ verkefni/app.py
@@ -1,75 +1,122 @@
-from flask import Flask, render_template, url_for, json 
-import urllib.request
-from random import randint,choice
-from datetime import date
-import subprocess
+from flask import Flask,render_template,request,session
+# mikið import fyrir wtformið
+import pyrebase
+from pyrebase.pyrebase import Auth
+
+from flask_wtf import FlaskForm
+from wtforms import StringField,EmailField,SubmitField,TextAreaField,IntegerField,HiddenField,DateField,PasswordField
+from wtforms.validators import DataRequired, InputRequired, Length
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "gaman" # þurfum þetta fyrir csrf, samt hafa random
+
 
 valmynd = [ {"url":'/',"hlekkur":"Heim"},
-            {"url":'/hlekkur/Enskar',"hlekkur":"Enskar"},
-            {"url":'/hlekkur/Íslenksar',"hlekkur":"Íslenksar"},
-            {"url":'/hlekkur/Top-20',"hlekkur":"Top 20"},
+            {"url":'/nyskra',"hlekkur":"nykra"},
+            {"url":'/innskra',"hlekkur":"innskra"},
+            {"url":'/utskra',"hlekkur":"útskra"},
           ]
 
 
 
+config = {
+  "apiKey": "AIzaSyBRjWDZ1B4Jfc4U11Bt2Cw2ft-oLVZpHIc",
+  "databaseURL": "",
+  "authDomain": "taekniskoli-48e6f.firebaseapp.com",
+  "projectId": "taekniskoli-48e6f",
+  "storageBucket": "taekniskoli-48e6f.appspot.com",
+  "messagingSenderId": "627254874069",
+  "appId": "1:627254874069:web:45363877c143d418f8375c",
+  "measurementId": "G-DHK5VMK936"
+}
+fb= pyrebase.initialize_app(config)
+auth = fb.auth()
 
-@app.route("/")
+# Skilgreinum Form klasann og innsláttarsvæðin, erfir frá FlaskForm
+class Frm(FlaskForm):
+    email = EmailField("Póstur:", validators=[InputRequired()])
+    password = PasswordField("lykilord", validators=[InputRequired()])
+    takki = SubmitField("skrá")
+
+@app.route('/')
 def index():
-    random_page = randint(1, 550)
+    # Tilvikið f af Frm klasanum ( sem er form ) sendur yfir í template
+    return render_template("index.html", f=Frm(), valmynd=valmynd)
 
-    with urllib.request.urlopen("https://api.themoviedb.org/3/discover/movie?api_key=2f4ea61d9bfaa42c92e84e4e34bd154b&page="+ str(random_page)) as api:
-        gogn = json.loads(api.read().decode())
-    return render_template("index.html", myndir=gogn, valmynd=valmynd)
+@app.route("/nyskra")
+def nykraupp():
+    return render_template("nyskra.html", f=Frm(), valmynd=valmynd)
 
-@app.route("/hlekkur/<nafn>")
-def hlekkur(nafn):
-    if nafn == "Enskar":
-        with urllib.request.urlopen("https://api.themoviedb.org/3/discover/movie?api_key=2f4ea61d9bfaa42c92e84e4e34bd154b&with_original_language={}".format(nafn)) as api:
-            hlekkur = json.loads(api.read().decode())
-    if nafn == "Íslenksar":
-        with urllib.request.urlopen("https://api.themoviedb.org/3/discover/movie?api_key=2f4ea61d9bfaa42c92e84e4e34bd154b&with_original_language={}".format(nafn)) as api:
-            hlekkur = json.loads(api.read().decode())
-    if nafn == "Top-20":
-        pass
-    return render_template("hlekkur.html", h=hlekkur,valmynd=valmynd)
-
-@app.route("/mynd/<id>")
-def mynd(id):
-    with urllib.request.urlopen("https://api.themoviedb.org/3/movie/{}?api_key=2f4ea61d9bfaa42c92e84e4e34bd154b&language=is-IS".format(id)) as api:
-        gogn = json.loads(api.read().decode())
+@app.route('/nyskra_vinnsla', methods=["POST", "GET"])
+def gogn():
+    # Förum bara hingað inn ef ýtt er á takkann úr forminu...
+    if request.form:
+        # tökum gögnin úr forminu og setjum í lista, sendum í template data.html
+        fr = Frm()
+        email = fr.email.data
+        password = fr.password.data
         
-        if gogn['overview'] == "":
-            with urllib.request.urlopen("https://api.themoviedb.org/3/movie/{}?api_key=2f4ea61d9bfaa42c92e84e4e34bd154b&language=en-US".format(id)) as api:
-                gogn = json.loads(api.read().decode())
-    
-    d = gogn['release_date']
-    d = date.fromisoformat(d)
-    d = (d.strftime("%#d.%#m.%Y"))
-
-    with urllib.request.urlopen("https://api.themoviedb.org/3/movie/{}/videos?api_key=2f4ea61d9bfaa42c92e84e4e34bd154b&language=en-US".format(id)) as api:
-        T = json.loads(api.read().decode())
-
-    
-    
-    with urllib.request.urlopen("https://api.themoviedb.org/3/movie/{}/credits?api_key=2f4ea61d9bfaa42c92e84e4e34bd154b&language=en-US".format(id)) as api:
-        c = json.loads(api.read().decode())
-
-    return render_template("myndir.html", d = d, myndir=gogn, valmynd=valmynd, t=T, c=c)
-
-@app.route("/Flokkar/<id>")
-def flokkar(id):
-    random_page = randint(1, 550)
-
-    with urllib.request.urlopen("https://api.themoviedb.org/3/discover/movie?with_genres={}&api_key=2f4ea61d9bfaa42c92e84e4e34bd154b&language=en-US&page="+str(random_page).format(id)) as api:
-        genrasapi = json.loads(api.read().decode())
+        try:
+            auth.create_user_with_email_and_password(email,password)
+            x = "Nýskráning tóks..."
+            return render_template("index.html",x=x)
+        except:
+            x = "Nýskráning tóks ekki..."
+            return render_template("index.html",x=x)
         
-    return render_template("Flokkar.html",ga=genrasapi,valmynd=valmynd)
+    else:
+        x= "Má ekki, verðum að koma úr formi"
+        return render_template("index.html",x=x)
 
-@app.errorhandler(404)
-def error(error):
-    return render_template("error.html")
 
-app.run(debug=True)
+@app.route('/innskra')
+def innskra():
+    # Tilvikið f af Frm klasanum ( sem er form ) sendur yfir í template
+    return render_template("innskra.html", f=Frm(), valmynd=valmynd )
+
+@app.route('/innskraning_vinnsla', methods=["POST", "GET"])
+def iv():
+    # Förum bara hingað inn ef ýtt er á takkann úr forminu...
+    if request.form:
+        # tökum gögnin úr forminu og setjum í lista, sendum í template data.html
+        fr = Frm()
+        email = fr.email.data
+        password = fr.password.data
+        
+        try:
+            notandi = auth.sign_in_with_email_and_password(email,password)
+            notandi_nanar = auth.get_account_info(notandi['idToken'])
+            print(notandi_nanar)
+            session['notandi'] = True 
+            
+            x = "Innskráning tóks..."
+            return render_template("index.html",x=x)
+        except:
+            x = "Innskráning tóks ekki..."
+            return render_template("index.html",x=x)
+        
+    else:
+        x = "Má ekki, verðum að koma úr formi"
+        return render_template("index.html",x=x)
+
+@app.route('/utskra')
+def utskra():
+    session.pop("notandi", None)
+    return "Þú ert útskráður"
+
+
+@app.route('/admin')
+def admin():
+    try:
+        if session['notandi'] == True:
+            return render_template("admin.html")
+    except:
+        x = "þetta gett ekki" 
+        return render_template("index.html",x=x)
+
+
+#render_template("data.html", d=d),
+
+if __name__ == "__main__":
+    app.run(debug=True)
