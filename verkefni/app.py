732303b2bdc893db6a95210904797d033bb81cdd
TTedy <breki121212@gmail.com>
2023-02-13 15:19:34 +0000

    224

--- verkefni/app.py
+++ verkefni/app.py
@@ -22,8 +22,8 @@ valmynd = [ {"url":'/',"hlekkur":"Heim"},
 
 config = {
   "apiKey": "AIzaSyBRjWDZ1B4Jfc4U11Bt2Cw2ft-oLVZpHIc",
-  "databaseURL": "",
   "authDomain": "taekniskoli-48e6f.firebaseapp.com",
+  "databaseURL": "https://taekniskoli-48e6f-default-rtdb.europe-west1.firebasedatabase.app",
   "projectId": "taekniskoli-48e6f",
   "storageBucket": "taekniskoli-48e6f.appspot.com",
   "messagingSenderId": "627254874069",
@@ -31,92 +31,46 @@ config = {
   "measurementId": "G-DHK5VMK936"
 }
 fb= pyrebase.initialize_app(config)
-auth = fb.auth()
+db = fb.database()
 
-# Skilgreinum Form klasann og innsláttarsvæðin, erfir frá FlaskForm
-class Frm(FlaskForm):
-    email = EmailField("Póstur:", validators=[InputRequired()])
-    password = PasswordField("lykilord", validators=[InputRequired()])
-    takki = SubmitField("skrá")
+class Dataform(FlaskForm):
+    email = EmailField("Netfang:",validators=[InputRequired()])
+    nafn = StringField("nafn:",validators=[InputRequired()])
+    takki = SubmitField("innskra:")
 
 @app.route('/')
 def index():
-    # Tilvikið f af Frm klasanum ( sem er form ) sendur yfir í template
-    return render_template("index.html", f=Frm(), valmynd=valmynd)
-
-@app.route("/nyskra")
-def nykraupp():
-    return render_template("nyskra.html", f=Frm(), valmynd=valmynd)
-
-@app.route('/nyskra_vinnsla', methods=["POST", "GET"])
-def gogn():
-    # Förum bara hingað inn ef ýtt er á takkann úr forminu...
-    if request.form:
-        # tökum gögnin úr forminu og setjum í lista, sendum í template data.html
-        fr = Frm()
-        email = fr.email.data
-        password = fr.password.data
-        
-        try:
-            auth.create_user_with_email_and_password(email,password)
-            x = "Nýskráning tóks..."
-            return render_template("index.html",x=x)
-        except:
-            x = "Nýskráning tóks ekki..."
-            return render_template("index.html",x=x)
-        
-    else:
-        x= "Má ekki, verðum að koma úr formi"
-        return render_template("index.html",x=x)
-
-
-@app.route('/innskra')
-def innskra():
-    # Tilvikið f af Frm klasanum ( sem er form ) sendur yfir í template
-    return render_template("innskra.html", f=Frm(), valmynd=valmynd )
-
-@app.route('/innskraning_vinnsla', methods=["POST", "GET"])
-def iv():
-    # Förum bara hingað inn ef ýtt er á takkann úr forminu...
-    if request.form:
-        # tökum gögnin úr forminu og setjum í lista, sendum í template data.html
-        fr = Frm()
-        email = fr.email.data
-        password = fr.password.data
-        
-        try:
-            notandi = auth.sign_in_with_email_and_password(email,password)
-            notandi_nanar = auth.get_account_info(notandi['idToken'])
-            print(notandi_nanar)
-            session['notandi'] = True 
-            
-            x = "Innskráning tóks..."
-            return render_template("index.html",x=x)
-        except:
-            x = "Innskráning tóks ekki..."
-            return render_template("index.html",x=x)
-        
-    else:
-        x = "Má ekki, verðum að koma úr formi"
-        return render_template("index.html",x=x)
-
-@app.route('/utskra')
-def utskra():
-    session.pop("notandi", None)
-    return "Þú ert útskráður"
-
-
-@app.route('/admin')
-def admin():
     try:
-        if session['notandi'] == True:
-            return render_template("admin.html")
+
+        user = db.child("Gogn").get()
+        getdata = dict(user.val())
+        return render_template("index.html", gd = getdata)
     except:
-        x = "þetta gett ekki" 
-        return render_template("index.html",x=x)
+        return "villa kom"
+
+    return render_template("index.html",valmynd=valmynd)
 
 
-#render_template("data.html", d=d),
+@app.route('/dataform')
+def df():
+    # Tilvikið f af Frm klasanum ( sem er form ) sendur yfir í template
+    return render_template("nyskra.html", df=Dataform(), valmynd=valmynd)
+
+@app.route('/nyskra', methods=["GET","POST"])
+def ns():
+    df = Dataform()
+    if request.form:
+        email = df.email.data
+        nafn = df.nafn.data
+        try:
+            db.child("Gogn").push({"email":email,"nafn":nafn})
+            return "gögn eru komin!"
+        except:
+            return "að seta gögn virkaði ekki :("
+    else:
+        return "það má ekki koma nema úr formi!!"
+    # Tilvikið f af Frm klasanum ( sem er form ) sendur yfir í template
+    return render_template("nyskra.html", df=Dataform(), valmynd=valmynd)
 
 if __name__ == "__main__":
     app.run(debug=True)
